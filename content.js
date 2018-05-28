ca972b58319f08ff5a57bc6834887f94f7874684
MaximPerry <maxim_perry@live.ca>
2018-05-28 08:27:05 -0400

    Initial commit

---
+++ content.js
@@ -0,0 +1,8 @@
+console.log("Facebook Camouflage successfully loaded");
+
+var topBar = document.getElementById("bluebarRoot");
+
+var ESDCLogo = document.createElement("img");
+ESDCLogo.src = "https://canadacompany.ca/canadacompany/_cascade/images/partner-organizations-logos/ESDCLogo.png";
+
+insertBefore(ESDCLogo, topBar);
